16ab98c92a05de4ee2ae9d4a77540f92ff623b90
Daniel-Chicas <70614489+Daniel-Chicas@users.noreply.github.com>
2020-09-09 23:38:01 -0600

    Create TorneoController.cs

---
+++ Controllers/TorneoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TorneoController : Controller
+    {
+        public ActionResult Torneo()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
