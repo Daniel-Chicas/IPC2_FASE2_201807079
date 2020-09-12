56531e5032ac7a3103722dff919c7cfba63cac1f
Daniel-Chicas <70614489+Daniel-Chicas@users.noreply.github.com>
2020-09-12 16:23:30 -0600

    Update CargarController.cs

--- Controllers/CargarController.cs
+++ Controllers/CargarController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Xml;
 using WebApplication1.Models;
+using System.Data;
+using System.Data.Sql;
 
 namespace WebApplication1.Controllers
 {
@@ -29,32 +31,32 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        //public ActionResult Leer(string pruta)
-        //{
-        //    var ArrayFichas = new List<Ficha>();
-        //    XmlTextReader lector = new XmlTextReader(pruta);
-        //    while (lector.Read())
-        //    {
-        //        if (lector.IsStartElement())
-        //        {
-        //            Ficha temp = new Ficha();
-        //            switch (lector.Name.ToString())
-        //            {
-        //                case "color":
-        //                    //color.Append(lector.ReadString());
-        //                    temp.color = lector.ReadString();
-        //                    break;
-        //                case "columna":
-        //                    //tx1.AppendText("color de la fruta: " + lector.ReadString() + "\r\n");
-        //                    temp.columna = lector.ReadString();
-        //                    break;
-        //                case "fila":
-        //                    //tx1.AppendText("tamanio de la fruta: " + lector.ReadString() + "\r\n\r\n");
-        //                    temp.fila = int.Parse(lector.ReadString());
-        //                    break;
-        //            }
-        //            ArrayFichas.Add(temp);
-        //        }
+        public ActionResult Leer(string pruta)
+        {
+            var ArrayFichas = new List<Ficha>();
+            XmlTextReader lector = new XmlTextReader(pruta);
+            while (lector.Read())
+            {
+                if (lector.IsStartElement())
+                {
+                    Ficha temp = new Ficha();
+                    switch (lector.Name.ToString())
+                    {
+                        case "color":
+                            //color.Append(lector.ReadString());
+                            temp.color = lector.ReadString();
+                            break;
+                        case "columna":
+                            //tx1.AppendText("color de la fruta: " + lector.ReadString() + "\r\n");
+                            temp.columna = lector.ReadString();
+                            break;
+                        case "fila":
+                            //tx1.AppendText("tamanio de la fruta: " + lector.ReadString() + "\r\n\r\n");
+                            temp.fila = int.Parse(lector.ReadString());
+                            break;
+                    }
+                    ArrayFichas.Add(temp);
+                }
 
         //        foreach (var item in ArrayFichas)
         //        {
@@ -62,8 +64,8 @@ namespace WebApplication1.Controllers
 
         //        }
                 
-        //    }
-        //    return View();
-        //}
+            }
+            return View();
+        }
     }
 }
