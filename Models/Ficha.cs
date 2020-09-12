e4851bc1388da4f4aeabef4bdc520284aea9f945
Daniel-Chicas <70614489+Daniel-Chicas@users.noreply.github.com>
2020-09-11 20:02:08 -0600

    Create Ficha.cs

---
+++ Models/Ficha.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Ficha
+    {
+        public string color { get; set; }
+        public string columna { get; set; }
+        public int fila { get; set; }
+    }
+}
