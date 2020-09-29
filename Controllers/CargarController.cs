621bb79b638cd4c03bacdea9a734e4e041246744
Daniel-Chicas <70614489+Daniel-Chicas@users.noreply.github.com>
2020-09-29 00:56:22 -0600

    Update CargarController.cs

--- Controllers/CargarController.cs
+++ Controllers/CargarController.cs
@@ -16,52 +16,5 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult Leer(string pruta)
-        {
-            var ArrayFichas = new List<Ficha>();
-            XmlReader reader = XmlReader.Create(pruta);
-            while (reader.Read())
-            {
-                if (reader.IsStartElement())
-                {
-                    Ficha temp = new Ficha();
-                    switch (reader.Name.ToString())
-                    {
-                        case "tablero":
-                            break;
-                        case "ficha":
-                            break;
-                        case "color":
-                            temp.color = reader.ReadString();
-                            break;
-                        case "columna":
-                            temp.columna = reader.ReadString();
-                            break;
-                        case "fila":
-                            temp.fila = reader.ReadElementContentAsInt();
-                            break;
-                        case "siguientetiro":
-                            break;
-                    }
-                }
-                
-            }
-            return View();
-        }
     }
 }
