09d241d60c40128243c1430acb8b4361ba7b9250
Daniel-Chicas <70614489+Daniel-Chicas@users.noreply.github.com>
2020-11-01 23:09:24 -0600

    Update TorneoController.cs

--- Controllers/TorneoController.cs
+++ Controllers/TorneoController.cs
@@ -3,21 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
+using WebApplication1.Models;
+using Microsoft.Ajax.Utilities;
+using System.Data;
+using System.Data.Sql;
 
 namespace WebApplication1.Controllers
 {
     public class TorneoController : Controller
     {
-        public ActionResult Torneo()
+        XmlDocument doc = new XmlDocument();
+        List<string> Ficha;
+        public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Torneo()
         {
-            ViewBag.Message = "Your application description page.";
-
+            Session["Campeonato"] = "";
             return View();
+        }
+
+        public string NombreTorneo(string nombre, int cantidad)
+        {
+            string cadena = "El campeonato \""+nombre+"\" ha sido creado.";
+            Session["Campeonato"] = nombre;
+            Session["cantidadIntegrantes"] = cantidad;
+            return cadena;
+        }
+
+        public ActionResult Jugadores()
+        {
+            return View();
+        }
+
+
+
+
+        public string Integrantes()
+        {
+            string cadena = Session["cantidadIntegrantes"].ToString();
+            return cadena;
+        }
+
+        public ActionResult Contra()
+        {
+            Session["contar"] = 0;
+            return View();
+        }
+
+        public string Conteo()
+        {
+            string cadena;
+            int cuenta = (int)Session["contar"];
+            cuenta++;
+            if (cuenta % 2 == 0)
+            {
+                cadena = "white";
+            }
+            else
+            {
+                cadena = "black";
+            }
+            Session["contar"] = cuenta;
+            return cadena + "&" + cuenta;
         }
 
         public ActionResult Contact()
@@ -25,6 +76,161 @@ namespace WebApplication1.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public string Leer(string pruta, int conteo)
+        {
+
+            conteo = conteo - 1;
+            string cadena = "0&No se completó la carga";
+            Ficha = new List<string>();
+            if (pruta == "")
+            {
+                cadena = "";
+            }
+            else
+            {
+                try
+                {
+                    Torneo temp = new Torneo();
+                    XmlReader reader = XmlReader.Create(pruta);
+                    List<List<string>> datos = new List<List<string>> { };
+                    List<string> info = new List<string> { };
+                    string NombreCampeonato = "";
+                    int contador = 0;
+                    string jugador = "";
+                    string nombreEquipo = "";
+                    while (reader.Read() != false)
+                    {
+                        if (reader.IsStartElement())
+                        {
+                            switch (reader.Name.ToString())
+                            {
+                                case "campeonato":
+                                    break;
+                                case "nombre":
+                                    temp.nombreCampeonato = reader.ReadString();
+                                    NombreCampeonato = temp.nombreCampeonato;
+                                    Session["Campeonato"] = NombreCampeonato;
+                                    break;
+                                case "equipo":
+                                    contador++;
+                                    if (info.Count() != 0)
+                                    {
+                                        List<string> ingresa = (List<string>)info;
+                                        datos.Add(ingresa);
+                                        info = new List<string>{ };
+                                    }
+                                    break;
+                                case "nombreEquipo":
+                                    if(contador == 1)
+                                    {
+                                        temp.nombreEquipo1 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo1;
+                                    }
+                                    if (contador == 2)
+                                    {
+                                        temp.nombreEquipo2 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo2;
+                                    }
+                                    if (contador == 3)
+                                    {
+                                        temp.nombreEquipo3 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo3;
+                                    }
+                                    if (contador == 4)
+                                    {
+                                        temp.nombreEquipo4 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo4;
+                                    }
+                                    if (contador == 5)
+                                    {
+                                        temp.nombreEquipo5 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo5;
+                                    }
+                                    if (contador == 6)
+                                    {
+                                        temp.nombreEquipo6 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo6;
+                                    }
+                                    if (contador == 7)
+                                    {
+                                        temp.nombreEquipo7 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo7;
+                                    }
+                                    if (contador == 8)
+                                    {
+                                        temp.nombreEquipo8 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo8;
+                                    }
+                                    if (contador == 9)
+                                    {
+                                        temp.nombreEquipo9 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo9;
+                                    }
+                                    if (contador == 10)
+                                    {
+                                        temp.nombreEquipo10 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo10;
+                                    }
+                                    if (contador == 11)
+                                    {
+                                        temp.nombreEquipo11 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo11;
+                                    }
+                                    if (contador == 12)
+                                    {
+                                        temp.nombreEquipo12 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo12;
+                                    }
+                                    if (contador == 13)
+                                    {
+                                        temp.nombreEquipo13 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo13;
+                                    }
+                                    if (contador == 14)
+                                    {
+                                        temp.nombreEquipo14 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo14;
+                                    }
+                                    if (contador == 15)
+                                    {
+                                        temp.nombreEquipo15 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo15;
+                                    }
+                                    if (contador == 16)
+                                    {
+                                        temp.nombreEquipo16 = reader.ReadString();
+                                        nombreEquipo = temp.nombreEquipo16;
+                                    }
+                                    info.Add(nombreEquipo);
+                                    break;
+                                case "jugador":
+                                    temp.jugadores1 = reader.ReadString();
+                                    jugador = temp.jugadores1;
+                                    info.Add(jugador);
+                                    break;
+                            }
+                        }
+                    }
+                    datos.Add(info);
+                    cadena = "La archivo fue cargado con éxito.";
+
+                    Session["lectura"] = datos;
+                }
+                catch (Exception)
+                {
+                    cadena = "El archivo no pudo ser cargado.";
+                    return cadena;
+                }
+            }
+            return cadena;
+        }
+
+        public object usuario()
+        {
+            var cadena = Session["usuario"];
+            return cadena;
         }
     }
 }
