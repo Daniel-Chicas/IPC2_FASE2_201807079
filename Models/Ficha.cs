786e39eae2175d1c9cc4d66f5c621d8ed2b73de7
Daniel-Chicas <70614489+Daniel-Chicas@users.noreply.github.com>
2020-11-01 23:09:48 -0600

    Update Ficha.cs

--- Models/Ficha.cs
+++ Models/Ficha.cs
@@ -20,4 +20,29 @@ namespace WebApplication1.Models
         public string colores2 { get; set; }
         public string modalidad { get; set; }
     }
+
+    public class Torneo
+    {
+        public string nombreCampeonato { get; set; }
+        public string nombreEquipo1 { get; set; }
+        public string nombreEquipo2 { get; set; }
+        public string nombreEquipo3 { get; set; }
+        public string nombreEquipo4 { get; set; }
+        public string nombreEquipo5 { get; set; }
+        public string nombreEquipo6 { get; set; }
+        public string nombreEquipo7 { get; set; }
+        public string nombreEquipo8 { get; set; }
+        public string nombreEquipo9 { get; set; }
+        public string nombreEquipo10 { get; set; }
+        public string nombreEquipo11 { get; set; }
+        public string nombreEquipo12 { get; set; }
+        public string nombreEquipo13 { get; set; }
+        public string nombreEquipo14 { get; set; }
+        public string nombreEquipo15 { get; set; }
+        public string nombreEquipo16 { get; set; }
+        public string jugadores1 { get; set; }
+        public string jugadores2 { get; set; }
+        public string jugadores3 { get; set; }
+       
+    }
 }
